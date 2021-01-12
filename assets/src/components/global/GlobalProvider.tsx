1c233d282b4b181f9680bbab5d3e3532851e6dd4
manhtai <vo@manhtai.com>
2021-01-12 22:03:03 +0700

    Wireframes

---
+++ assets/src/components/global/GlobalProvider.tsx
@@ -0,0 +1,50 @@
+import React, {useContext} from 'react';
+import { SyncStatus } from 'src/libs/types';
+
+export const GlobalContext = React.createContext<{
+  sync: SyncStatus;
+  error: any;
+
+  setSync: (...a: any) => void;
+}>({
+  sync: SyncStatus.Success,
+  error: null,
+
+  setSync: () => {},
+});
+
+export const useGlobal = () => useContext(GlobalContext);
+
+type Props = React.PropsWithChildren<{}>;
+type State = {
+  sync: SyncStatus;
+  error: string;
+};
+
+export class GlobalProvider extends React.Component<Props, State> {
+  state: State = {
+    sync: SyncStatus.Success,
+    error: '',
+  };
+
+  setSync(sync: SyncStatus, error: any) {
+    this.setState({ sync, error })
+  }
+
+  render() {
+    const {sync, error} = this.state;
+
+    return (
+      <GlobalContext.Provider
+        value={{
+          sync,
+          error,
+
+          setSync: this.setSync,
+        }}
+      >
+        {this.props.children}
+      </GlobalContext.Provider>
+    );
+  }
+}
