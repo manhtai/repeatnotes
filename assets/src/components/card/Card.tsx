307cbc9148326832aed06aeb0007406a8f31635e
manhtai <vo@manhtai.com>
2021-01-20 09:40:43 +0700

    Working POC

--- assets/src/components/card/Card.tsx
+++ assets/src/components/card/Card.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {useCard, CardProvider} from './CardProvider';
-import {Choice} from 'src/libs/types';
+import {Choice, CardQueue} from 'src/libs/types';
 import logger from 'src/libs/logger';
 
 function Card() {
@@ -19,12 +19,15 @@ function Card() {
     lapses: 0,
     remaining_steps: 0,
   };
-  const [card, setCard] = useState(cardDefault)
+  const [card, setCard] = useState(cardDefault);
 
   const answerCard = (choice: Choice) => {
-    logger.info("Answer with choice", choice);
-    sm2.answer_card(card, choice);
-    setCard({ ...card, id: Math.random() })
+    logger.info('Answer with choice', choice);
+    const newCard = sm2.answer_card(card, choice);
+    setCard({...newCard, id: 1234});
+    if (newCard.card_queue === CardQueue.Suspended) {
+      alert('Card is suspended!');
+    }
   };
 
   const nextInterval = (choice: Choice) => {
@@ -40,17 +43,20 @@ function Card() {
           <div className="flex text-center">
             <button
               className="flex-1 w-full px-3 py-1 mb-1 mr-1 font-bold text-gray-200 bg-red-700 rounded-full outline-none active:bg-red-500 hover:bg-red-600 focus:outline-none"
-              onClick={() => answerCard(Choice.Again)}>
+              onClick={() => answerCard(Choice.Again)}
+            >
               Again ({nextInterval(Choice.Again)})
             </button>
             <button
               className="flex-1 w-full px-3 py-1 mb-1 mr-1 font-bold text-gray-200 bg-yellow-700 rounded-full outline-none active:bg-yellow-500 hover:bg-yellow-600 focus:outline-none"
-              onClick={() => answerCard(Choice.Hard)}>
+              onClick={() => answerCard(Choice.Hard)}
+            >
               Hard ({nextInterval(Choice.Hard)})
             </button>
             <button
               className="flex-1 w-full px-3 py-1 mb-1 mr-1 font-bold text-gray-100 bg-green-700 rounded-full outline-none active:bg-green-500 hover:bg-green-600 focus:outline-none"
-              onClick={() => answerCard(Choice.Ok)}>
+              onClick={() => answerCard(Choice.Ok)}
+            >
               Ok ({nextInterval(Choice.Ok)})
             </button>
             <button
