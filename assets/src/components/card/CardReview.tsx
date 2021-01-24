98f4525236def8d02fce65c67f0a3ab01ee2ec01
manhtai <vo@manhtai.com>
2021-01-24 13:59:03 +0700

    Some touches

--- assets/src/components/card/CardReview.tsx
+++ assets/src/components/card/CardReview.tsx
@@ -1,11 +1,63 @@
 import React, {useEffect, useState} from 'react';
 import {useSrs, SrsProvider} from './SrsProvider';
 import {useGlobal} from 'src/components/global/GlobalProvider';
-import {Choice, SyncStatus, Card, EditorTab} from 'src/libs/types';
+import {Choice, SyncStatus, SrsConfig, Card, EditorTab} from 'src/libs/types';
 import logger from 'src/libs/logger';
 import * as API from 'src/libs/api';
 
 import Editor from 'src/components/editor/MarkdownEditor';
+
+type AnswerProps = {
+  card: Card;
+  config?: SrsConfig;
+  answerCard: (card: Card, choice: Choice) => void;
+  nextInterval: (card: Card, choice: Choice) => string;
+};
+
+function Answer(props: AnswerProps) {
+  const {card, config, answerCard, nextInterval} = props;
+
+  return (
+    <div className="flex items-center justify-center mt-2">
+      <button
+        className="flex-1 w-full px-3 py-1 mb-1 mr-1 font-bold text-gray-200 bg-red-700 rounded-full outline-none active:bg-red-500 hover:bg-red-600 focus:outline-none"
+        onClick={() => answerCard(card, Choice.Again)}
+      >
+        Again
+        {config?.show_next_due && (
+          <span> ({nextInterval(card, Choice.Again)})</span>
+        )}
+      </button>
+      <button
+        className="flex-1 w-full px-3 py-1 mb-1 mr-1 font-bold text-gray-200 bg-yellow-700 rounded-full outline-none active:bg-yellow-500 hover:bg-yellow-600 focus:outline-none"
+        onClick={() => answerCard(card, Choice.Hard)}
+      >
+        Hard
+        {config?.show_next_due && (
+          <span> ({nextInterval(card, Choice.Hard)})</span>
+        )}
+      </button>
+      <button
+        className="flex-1 w-full px-3 py-1 mb-1 mr-1 font-bold text-gray-100 bg-green-700 rounded-full outline-none active:bg-green-500 hover:bg-green-600 focus:outline-none"
+        onClick={() => answerCard(card, Choice.Ok)}
+      >
+        Good
+        {config?.show_next_due && (
+          <span> ({nextInterval(card, Choice.Ok)})</span>
+        )}
+      </button>
+      <button
+        className="flex-1 w-full px-3 py-1 mb-1 mr-1 font-bold text-gray-100 bg-blue-700 rounded-full outline-none active:bg-blue-500 hover:bg-blue-600 focus:outline-none"
+        onClick={() => answerCard(card, Choice.Easy)}
+      >
+        Easy
+        {config?.show_next_due && (
+          <span> ({nextInterval(card, Choice.Easy)})</span>
+        )}
+      </button>
+    </div>
+  );
+}
 
 function CardReview() {
   const {sm2, loadSm2, loading, config} = useSrs();
@@ -14,6 +66,7 @@ function CardReview() {
   const [card, setCard] = useState<Card | null>(null);
   const [cards, setCards] = useState([]);
   const [selectedTab, setSelectedTab] = useState<EditorTab>('preview');
+  const [fetchingCards, setFetchingCards] = useState(true);
 
   const answerCard = async (card: Card, choice: Choice) => {
     const newCard = sm2.answer_card(card, choice);
@@ -35,27 +88,43 @@ function CardReview() {
     }
   };
 
-  const nextInterval = (choice: Choice) => {
+  const nextInterval = (card: Card, choice: Choice) => {
     return sm2.next_interval_string(card, choice);
   };
 
   const fetchAllCards = () => {
+    setFetchingCards(true);
     sm2 &&
-      API.fetchAllCards(sm2.day_today()).then((cards) => {
-        setCards(cards);
-        if (cards.length) {
-          setCard(cards[0]);
+      API.fetchAllCards(sm2.day_today()).then(
+        (cards) => {
+          setCards(cards);
+          if (cards.length) {
+            setCard(cards[0]);
+          }
+          setFetchingCards(false);
+        },
+        (error) => {
+          logger.error(error);
+          setFetchingCards(false);
         }
-      });
+      );
   };
 
   useEffect(loadSm2, [loadSm2]);
 
   useEffect(fetchAllCards, [sm2]);
 
+  if (loading) {
+    return null;
+  }
+
+  if (fetchingCards) {
+    return null;
+  }
+
   return (
-    <div className="mt-10">
-      {loading ? null : !card ? (
+    <div className="mt-5">
+      {!card ? (
         <div className="px-4 py-3 border rounded shadow">
           No card to learn for now, come back here later.
         </div>
@@ -74,44 +143,14 @@ function CardReview() {
             />
           </div>
 
-          <div className="flex items-center justify-center mt-2">
-            <button
-              className="flex-1 w-full px-3 py-1 mb-1 mr-1 font-bold text-gray-200 bg-red-700 rounded-full outline-none active:bg-red-500 hover:bg-red-600 focus:outline-none"
-              onClick={() => answerCard(card, Choice.Again)}
-            >
-              Again
-              {config?.show_next_due && (
-                <span> ({nextInterval(Choice.Again)})</span>
-              )}
-            </button>
-            <button
-              className="flex-1 w-full px-3 py-1 mb-1 mr-1 font-bold text-gray-200 bg-yellow-700 rounded-full outline-none active:bg-yellow-500 hover:bg-yellow-600 focus:outline-none"
-              onClick={() => answerCard(card, Choice.Hard)}
-            >
-              Hard
-              {config?.show_next_due && (
-                <span> ({nextInterval(Choice.Hard)})</span>
-              )}
-            </button>
-            <button
-              className="flex-1 w-full px-3 py-1 mb-1 mr-1 font-bold text-gray-100 bg-green-700 rounded-full outline-none active:bg-green-500 hover:bg-green-600 focus:outline-none"
-              onClick={() => answerCard(card, Choice.Ok)}
-            >
-              Good
-              {config?.show_next_due && (
-                <span> ({nextInterval(Choice.Ok)})</span>
-              )}
-            </button>
-            <button
-              className="flex-1 w-full px-3 py-1 mb-1 mr-1 font-bold text-gray-100 bg-blue-700 rounded-full outline-none active:bg-blue-500 hover:bg-blue-600 focus:outline-none"
-              onClick={() => answerCard(card, Choice.Easy)}
-            >
-              Easy
-              {config?.show_next_due && (
-                <span> ({nextInterval(Choice.Easy)})</span>
-              )}
-            </button>
-          </div>
+          {selectedTab === 'preview' && (
+            <Answer
+              card={card}
+              answerCard={answerCard}
+              nextInterval={nextInterval}
+              config={config}
+            />
+          )}
         </div>
       )}
     </div>
