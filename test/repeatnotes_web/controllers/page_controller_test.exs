f3b6f02131b3ca360a7a91e43707848e6a9279b4
Mạnh Tài <vo@manhtai.com>
2021-02-02 23:21:25 +0700

    CI (#32)

--- test/repeatnotes_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule RepeatNotesWeb.PageControllerTest do
-  use RepeatNotesWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
-  end
-end
