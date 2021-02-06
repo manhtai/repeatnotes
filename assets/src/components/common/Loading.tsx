28599aeecfcbed543159f22df0219cb92c8ef97e
manhtai <vo@manhtai.com>
2021-02-06 23:21:05 +0700

    Fix loading screen

--- assets/src/components/common/Loading.tsx
+++ assets/src/components/common/Loading.tsx
@@ -2,7 +2,7 @@ import LoadingIcon from 'src/img/loading.svg';
 
 export default function Loading() {
   return (
-    <div className="absolute flex items-center justify-center w-full h-full -mx-4 -mt-16 bg-gray-100 opacity-70">
+    <div className="absolute inset-0 flex items-center justify-center bottom-1/4">
       <img className="text-indigo-500" src={LoadingIcon} alt="Loading..." />
     </div>
   );
