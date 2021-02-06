eab6c4ad46d5875105408bfbb041d203e3f529d2
manhtai <vo@manhtai.com>
2021-02-06 22:30:59 +0700

    Padding for mobile

--- assets/src/components/common/Loading.tsx
+++ assets/src/components/common/Loading.tsx
@@ -2,7 +2,7 @@ import LoadingIcon from 'src/img/loading.svg';
 
 export default function Loading() {
   return (
-    <div className="absolute flex items-center justify-center w-full h-full bg-gray-100 opacity-70">
+    <div className="absolute flex items-center justify-center w-full h-full -mx-4 -mt-16 bg-gray-100 opacity-70">
       <img className="text-indigo-500" src={LoadingIcon} alt="Loading..." />
     </div>
   );
