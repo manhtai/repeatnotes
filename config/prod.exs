fb70ae3bfdd7787d4d6eca2b5124603107577a07
manhtai <vo@manhtai.com>
2021-02-10 15:47:31 +0700

    Change url to herokuapp.com

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :repeatnotes, RepeatNotesWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "repeatnotes.com", port: 443],
+  url: [scheme: "https", host: "repeatnotes.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
